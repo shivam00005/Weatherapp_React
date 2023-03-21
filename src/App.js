86f0b19e36afcce92ca7f8127cc2cd5a3613701e
Shivam Sharma <shivampramod95925@gmail.com>
2023-03-21 00:27:04 -0500

    styled custom components

--- src/App.js
+++ src/App.js
@@ -20,6 +20,32 @@ export const WeatherIcons = {
   "11d": "/react-weather-app/icons/storm.svg",
   "11n": "/react-weather-app/icons/storm.svg",
 };
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 380px;
+  padding: 20px 10px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 3px 6px 0 #555;
+  background: white;
+  font-family: Montserrat;
+`;
+
+const AppLabel = styled.span`
+  color: black;
+  margin: 20px auto;
+  font-size: 18px;
+  font-weight: bold;
+`;
+const CloseButton = styled.span`
+  padding: 2px 3px;
+  background-color: black;
+  border-radius: 50%;
+  color: white;
+  position: absolute;
+`;
 function App() {
   const [city, updateCity] = useState();
   const [weather, updateWeather] = useState();
