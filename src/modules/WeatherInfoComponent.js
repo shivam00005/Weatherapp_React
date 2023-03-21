c49c012a5bd82802da97bb05c24787d6c16daae6
Shivam Sharma <shivampramod95925@gmail.com>
2023-03-21 00:33:21 -0500

    imported wather icons to detail page

---
+++ src/modules/WeatherInfoComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import {WeatherIcons} from "../App";
+
+export const WeatherInfoIcons = {
+    sunset: "/react-weather-app/icons/temp.svg",
+    sunrise: "/react-weather-app/icons/temp.svg",
+    humidity: "/react-weather-app/icons/humidity.svg",
+    wind: "/react-weather-app/icons/wind.svg",
+    pressure: "/react-weather-app/icons/pressure.svg",
+};
